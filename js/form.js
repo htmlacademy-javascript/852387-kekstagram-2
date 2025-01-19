7eeb788f2d9f308e532a8af34af11b83ca9589aa
2tankov2 <domino-82@mail.ru>
2025-01-20 02:52:33 +0300

    добавляет сбрасывание полей формы при закрытии окна формы

--- js/form.js
+++ js/form.js
@@ -1,5 +1,4 @@
 import { isEscapeKey } from './util.js';
-// форма находиться в <section class="pictures  container">
 
 const body = document.querySelector('body');
 const form = document.querySelector('.img-upload__form');
@@ -7,6 +6,11 @@ const uploadFile = form.querySelector('.img-upload__input');
 const uploadModal = form.querySelector('.img-upload__overlay');
 
 const buttonClose = uploadModal.querySelector('.img-upload__cancel');
+
+const effectLevel = form.querySelector('.effect-level__value');
+const hashtags = form.querySelector('.text__hashtags');
+const description = form.querySelector('.text__description');
+const effectsList = form.querySelectorAll('input[type="radio"]');
 
 const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -26,15 +30,25 @@ const onChangeUploadFile = (evt) => {
 uploadFile.addEventListener('change', onChangeUploadFile);
 
 function closeUploadModal () {
+
+  uploadFile.value = null;
+  effectLevel.value = null;
+  hashtags.value = null;
+  description.value = null;
+  effectsList.forEach((effect) => {
+    if (effect.id['effect-none']) {
+      effect.checked = true;
+    } else {
+      effect.checked = false;
+    }
+  });
+
   uploadModal.classList.add('hidden');
   body.classList.remove('modal-open');
+
   document.removeEventListener('keydown', onDocumentKeydown);
 }
 
 buttonClose.addEventListener('click', () => {
   closeUploadModal();
 });
-
-// Закрытие формы редактирования изображения производится либо нажатием на
-// кнопку .img-upload__cancel, либо нажатием клавиши Esc. Элементу .img-upload__overlay
-//  возвращается класс hidden. У элемента body удаляется класс modal-open.
