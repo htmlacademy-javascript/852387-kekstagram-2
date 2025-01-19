f203b744539c9bbbadc2b2990c2b25e65b60eb52
2tankov2 <domino-82@mail.ru>
2025-01-19 05:58:27 +0300

    поправки в модулях добавления блока комментариев

--- js/comments.js
+++ js/comments.js
@@ -1,37 +1,69 @@
 import { createCountCommentsLoader } from './util.js';
 
-const commentContainer = document.querySelector('.social__comments');
-const commentSample = commentContainer.querySelector('.social__comment');
+const COMMENT_COUNT_LOADER = 5;
+let listComments = [];
+let startIndex = createCountCommentsLoader();
 
+const commentContainer = document.querySelector('.social__comments');
+const bigPicture = document.querySelector('.big-picture');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
+const commentSample = commentContainer.querySelector('.social__comment');
+const commentsCount = bigPicture.querySelector('.social__comment-count');
+const commentShownCount = commentsCount.querySelector('.social__comment-shown-count');
+const commentsList = bigPicture.querySelector('.social__comments');
 
 const renderCommit = function ({avatar, message, name}) {
+
   const commentElement = commentSample.cloneNode(true);
   const img = commentElement.querySelector('.social__picture');
   img.src = `${avatar}`;
   img.alt = `${name}`;
   const text = commentElement.querySelector('.social__text');
   text.textContent = `${message}`;
-
   return commentElement;
+
 };
 
-const getComments = function (comments, count) {
+const getComments = function () {
 
   const commentsListFragment = document.createDocumentFragment();
-  const startIndexComment = createCountCommentsLoader();
-  const showComment = comments.slice(startIndexComment, count);
+  const start = startIndex();
+  let count = COMMENT_COUNT_LOADER + start;
+  const showComment = listComments.slice(start, count);
 
-  if (comments.length < count || showComment.length < count) {
-    count = comments.length;
+  if (listComments.length < count || showComment.length < count) {
+    count = listComments.length;
   }
 
   showComment.forEach((comment) => {
     const renderCommitElement = renderCommit(comment);
     commentsListFragment.appendChild(renderCommitElement);
+
   });
 
-  commentContainer.innerHTML = '';
   commentContainer.appendChild(commentsListFragment);
+  commentShownCount.textContent = commentsList.children.length;
+
+  if (listComments.length === commentsList.children.length) {
+    commentsLoader.classList.add('hidden');
+  }
 };
 
-export { getComments };
+const renderComments = (currntComments) => {
+
+  commentContainer.innerHTML = '';
+  listComments = currntComments;
+  getComments();
+  commentsLoader.addEventListener('click', getComments);
+
+};
+
+const clearComments = function () {
+
+  startIndex = createCountCommentsLoader();
+  commentsLoader.classList.add('hidden');
+  commentsLoader.removeEventListener('click', getComments);
+
+};
+
+export { renderComments, clearComments };
